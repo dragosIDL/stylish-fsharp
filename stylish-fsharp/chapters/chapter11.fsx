43ce1648905c5942610bf475382b6f6b07253101
dragos.ismana <dragos.ismana@makingwaves.com>
2019-06-29 18:26:09 +0200

    added chapter 11 start

---
+++ stylish-fsharp/chapters/chapter11.fsx
@@ -0,0 +1,21 @@
+﻿(*
+    11-1 Reproducing maperror
+    Can you implement a passThroughRejects function, with the same behavior as the build-in mapError function?
+*)
+
+type Outcome<'TSuccess, 'TFailure> = 
+    | Success of 'TSuccess
+    | Failure of 'TFailure
+
+let adapt func = function
+| Success x -> func x
+| Failure f -> Failure f
+
+let passThrough func = function
+| Success x -> func x |> Success
+| Failure x -> Failure x
+
+let passThroughRejects func = function
+| Success x -> Success x 
+| Failure x -> func x |> Failure 
+ 
